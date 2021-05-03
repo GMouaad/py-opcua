0d8a8278c843863fca923e52dd4747b7b610048b
Mouaad Gssair <Mgsair@gmail.com>
2021-05-03 19:45:32 +0200

    small change

---
+++ client/main.py
@@ -0,0 +1,22 @@
+import json
+import time
+
+
+def start():
+    # client = Client()
+    #
+    # # Start the server
+    # client.start()
+    # print("Server started at {}".format(URL))
+    #
+    # try:
+    #     while True:
+    #         time.sleep(1)
+    # finally:
+    #     # close connection, etc
+    #     client.stop()
+    pass
+
+
+if __name__ == '__main__':
+    start()
